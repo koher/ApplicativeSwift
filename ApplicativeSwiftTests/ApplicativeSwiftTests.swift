2fdbc0843eeb235b5390d32e633662a2cc483c97
koher <koher@koherent.org>
2015-08-06 23:38:11 +0900

    Add the test of currying initializers

--- ApplicativeSwiftTests/ApplicativeSwiftTests.swift
+++ ApplicativeSwiftTests/ApplicativeSwiftTests.swift
@@ -36,6 +36,21 @@ class ApplicativeSwiftTests: XCTestCase {
         XCTAssertEqual((*) <%> a <*> b, [3, 4, 6, 8])
         XCTAssertEqual([curry(+), curry(*)] <*> a <*> b, [4, 5, 5, 6, 3, 4, 6, 8]) // in Haskell: [(+), (*)] <$> a <*> b
     }
+    
+    func testOptionalInitializer() {
+        let bar: Int = 2
+        let baz: Float = 3.0
+        let qux: String = "5"
+        
+        guard let foo = Foo.init <%> bar <*> baz <*> qux else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(foo.bar, 2)
+        XCTAssertEqual(foo.baz, 3.0)
+        XCTAssertEqual(foo.qux, "5")
+    }
 }
 
 func add3(a: Int, b: Int, c: Int) -> Int {
@@ -56,4 +71,10 @@ func add6(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) -> Int {
 
 func add7(a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) -> Int {
     return a + b + c + d + e + f + g
+}
+
+struct Foo {
+    let bar: Int
+    let baz: Float
+    let qux: String
 }
